d4421ee8a8c4a3a7514641f662371a7cf0b5650d
chema969 <chema969@hotmail.com>
2019-09-16 15:51:20 +0200

    area

--- practica1.rkt
+++ practica1.rkt
@@ -9,4 +9,25 @@
   (* 4.184 calorias))
 
 (define (julios_a_calorias julios)
-  (/ julios 4.184)) 
+  (/ julios 4.184))
+
+(define (celsius_a_farenheit oC)
+ (+ (* oC  1.8) 32))
+
+(define (farenheit_a_celsius oF)
+  (/ (- oF 32)  1.8))
+
+(define (semiperimetro a b c)
+  (/ (+ a b c) 2))
+
+(define (areaTriangulo a b c)
+  (sqrt
+   (*
+    (semiperimetro a b c)
+    (- (semiperimetro a b c) a)
+    (- (semiperimetro a b c) b)
+    (- (semiperimetro a b c) c)
+    )
+   )
+  )
+         
