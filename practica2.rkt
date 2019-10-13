e6ccf89e1df751c7219df9a6b6deeb8cc0d3afa0
chema969 <chema969@hotmail.com>
2019-10-13 19:16:11 +0200

    XD

--- practica2.rkt
+++ practica2.rkt
@@ -162,7 +162,7 @@
         (d2 (sqrt (+ (cuadrado v1) (cuadrado v2))))
         (m  (+ (* u1 v1) (* u2 v2)))
         )
-       (if (< (acos (/ m (* d1 d2))) 1e-6)
+       (if (< (abs(acos (/ m (* d1 d2)))) 1e-6)
            0
            (acos (/ m (* d1 d2)))
            )
@@ -232,7 +232,7 @@
        (v1 (- x3 x2))
        (v2 (- y3 y2))
        )
-    (if (= 0 (calcAng u1 u2 v1 v2))
+    (if (or (= 0 (calcAng u1 u2 v1 v2)) (= pi (calcAng u1 u2 v1 v2)))
         #t
         #f
         )
@@ -288,7 +288,20 @@
   (let(
        (paralelos1 (ladosParalelos? x0 y0 x1 y1 x2 y2 x3 y3))
        (paralelos2 (ladosParalelos? x0 y0 x2 y2 x1 y1 x3 y3))
-       (lado1 (D2 x0 y0 x1 y1))
-       (lado2 (D2 x0 y0 x2 y2))
-       (lado3 (D2 xd
+       (paralelos3 (ladosParalelos? x0 y0 x3 y3 x1 y1 x2 y2))
+       )
+    (if (or (and paralelos1 paralelos2) (and paralelos1 paralelos3) (and paralelos2 paralelos3))
+        (if  (not (or (and (perpendiculares? x0 y0 x1 y1 x0 y0 x2 y2) (perpendiculares? x3 y3 x1 y1 x3 y3 x2 y2))
+                      (and (perpendiculares? x0 y0 x1 y1 x0 y0 x3 y3) (perpendiculares? x2 y2 x1 y1 x2 y2 x3 y3))
+                      (and (perpendiculares? x3 y3 x1 y1 x3 y3 x0 y0) (perpendiculares? x2 y2 x1 y1 x2 y2 x0 y0))))
+             3
+             (if (not (= (D2 x0 y0 x1 y1) (D2 x2 y2 x3 y3) (D2 x0 y0 x2 y2) (D2 x1 y1 x3 y3)))
+                 2
+                 1
+                 )
+             )
+        0
+    )
+  )
+)    
           
